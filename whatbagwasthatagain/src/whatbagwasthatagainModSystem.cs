c90f074edf438a16ae6ceed5d22556c316c77f91
iubaldo <ianzachary.u@gmail.com>
2023-09-15 20:40:44 -0500

    implemented bag shaking upon mouseover

---
+++ whatbagwasthatagain/src/whatbagwasthatagainModSystem.cs
@@ -0,0 +1,49 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.Common;
+
+using System.Linq;
+
+
+namespace whatbagwasthatagain.src
+{
+    public class WBWTA_Main : ModSystem
+    {
+        ICoreClientAPI capi;
+
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(api);
+
+            capi = api;
+
+            capi.Event.RegisterGameTickListener(HighlightSlot, 1000);
+        }
+
+
+        public override void AssetsFinalize(ICoreAPI api)
+        {
+            base.AssetsFinalize(api);
+
+            api.Logger.StoryEvent("loading wbwta");
+        }
+
+
+        public void HighlightSlot(float deltaTime)
+        {
+            if (!capi.World.Player.InventoryManager.OpenedInventories.Any())
+                return;
+
+            if (capi.World.Player.InventoryManager.CurrentHoveredSlot == null || capi.World.Player.InventoryManager.CurrentHoveredSlot is not ItemSlotBackpackContent)
+                return;
+
+            ItemSlotBackpackContent currentSlot = capi.World.Player.InventoryManager.CurrentHoveredSlot as ItemSlotBackpackContent;
+            InventoryPlayerBackPacks backpackInv = capi.World.Player.InventoryManager.GetOwnInventory(GlobalConstants.backpackInvClassName) as InventoryPlayerBackPacks;
+
+            int id = currentSlot.BackpackIndex;
+            backpackInv.PerformNotifySlot(id);
+        }
+    }
+}
